82656c74da909529a52e56a848dbec13dd6ec273
tri135790 <tranthanhtrivn@gmail.com>
2022-07-19 22:21:43 +0700

    add level1

---
+++ src/leetcode75/level1/RunningSumOf1dArray.java
@@ -0,0 +1,19 @@
+package leetcode75.level1;
+
+public class RunningSumOf1dArray {
+
+    public static int[] runningSum(int[] nums) {
+
+        for (int i = 1 ; i < nums.length; i++) {
+                nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+
+    public static void main(String args[]) {
+        int[] nums = {1,2,3,4};
+        for (int i : runningSum(nums)) {
+            System.out.println(i);
+        }
+    }
+}
