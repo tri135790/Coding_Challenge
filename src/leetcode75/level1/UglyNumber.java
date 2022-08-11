1abf887ae50a9dec9557b4629b981ae70103dff2
tri135790 <tranthanhtrivn@gmail.com>
2022-08-11 15:45:34 +0700

    3rd per week

---
+++ src/leetcode75/level1/UglyNumber.java
@@ -0,0 +1,13 @@
+package leetcode75.level1;
+
+
+public class UglyNumber {
+
+    public boolean isUgly(int n) {
+        if (n == 0 ) return false;
+        while (n % 2 == 0) n = n/2;
+        while (n % 3 == 0) n = n/3;
+        while (n % 5 == 0) n = n/5;
+        return n == 1;
+    }
+}
