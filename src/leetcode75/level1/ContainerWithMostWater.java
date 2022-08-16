e2f3fa92950bd9cdc10460524a88348bd5ae89ee
tri135790 <tranthanhtrivn@gmail.com>
2022-08-16 22:18:19 +0700

    1st per week

---
+++ src/leetcode75/level1/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package leetcode75.level1;
+//Review
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right])
+                * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/leetcode75/level1/GenerateParentheses.java
@@ -0,0 +1,10 @@
+package leetcode75.level1;
+
+import java.util.List;
+//TODO
+public class GenerateParentheses {
+
+//    public List<String> generateParenthesis(int n) {
+//
+//    }
+}
