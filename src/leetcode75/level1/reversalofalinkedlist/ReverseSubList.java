084234020ce3c92484cff52880a7ec3f62c33e5b
tri135790 <tranthanhtrivn@gmail.com>
2022-10-13 09:12:24 +0700

    add

---
+++ src/leetcode75/level1/reversalofalinkedlist/ReverseSubList.java
@@ -0,0 +1,50 @@
+package leetcode75.level1.reversalofalinkedlist;
+
+public class ReverseSubList {
+
+    public static ListNode reverse(ListNode head, int p, int q) {
+        if (p == q)
+            return head;
+
+        ListNode current = head, previous = null;
+        for (int i = 0; current != null && i < p - 1; ++i) {
+            previous = current;
+            current = current.next;
+        }
+
+        ListNode lastNodeOfFirstPart = previous; // points to the node at index 'p-1'
+        ListNode lastNodeOfSubList = current;
+        ListNode next = null; // will be used to temporarily store the next node
+        for (int i = 0; current != null && i < q - p + 1; i++) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        if (lastNodeOfFirstPart != null)
+            lastNodeOfFirstPart.next = previous; // 'previous' is now the first node of the sub-list
+        else // this means p == 1 i.e., we are changing the first node (head) of the LinkedList
+            head = previous;
+
+        // connect with the last part
+        lastNodeOfSubList.next = current;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = ReverseSubList.reverse(head, 2, 4);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+}
